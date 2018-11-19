f084e349b3206f8c10da4d9a8b922bb2f0bfefda
Galen Rhodes <galenrhodes@me.com>
2018-11-19 11:07:16 -0500

    Initial Commit

---
+++ PGPBXToCMake/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PGPBXToCMake
+//
+//  Created by Galen Rhodes on 11/19/18.
+//  Copyright © 2018 Project Galen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
